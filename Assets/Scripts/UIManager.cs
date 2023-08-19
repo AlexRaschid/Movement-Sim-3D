5668de7eaf4e81d4d7128c3965a056c29ad2aaa7
AlexRaschid <AlexRaschid@users.noreply.github.com>
2023-08-18 20:29:51 -0400

    UI adjustment
    -named and repositioned InfoReport UI
    -added state to InfoReport

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+
 public class UIManager : MonoBehaviour
 {
     [SerializeField] 
-    public TMP_Text velocityTxt;
+    public TMP_Text infoReport;
+
+    //private PlayerMain playerMain;
     // Start is called before the first frame update
-    public void UpdateVelocityTxt(string v)
+    void Start()
     {
-        velocityTxt.text = "Velocity: " + v;
+        //playerMain = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMain>();
     }
+    public void InfoReport(string v, string state)
+    {
+        infoReport.text = "Velocity: " + v + "\n" +
+                        "State: " + state + "\n";
+    }
+
+    
 }
